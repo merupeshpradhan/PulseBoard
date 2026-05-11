3772feeafaca6f11703f0dcbd27ec83d5a5966ec
Rupesh Pradhan <merupeshpradhan@gmail.com>
2026-05-11 23:19:54 +0530

    Basic Setup for client

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,17 +1,13 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import './App.css'
+import { useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
-    <h1>Rupesh Pulse Board</h1>
+      <h1>Rupesh Pulse Board</h1>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
