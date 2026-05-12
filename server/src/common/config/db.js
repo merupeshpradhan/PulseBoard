4b3d610735540c95f8e69f79b9a9e672fc934176
Rupesh Pradhan <merupeshpradhan@gmail.com>
2026-05-12 23:45:04 +0530

    feat: add Google DNS configuration to resolve MongoDB connection issues

--- server/src/common/config/db.js
+++ server/src/common/config/db.js
@@ -1,9 +1,17 @@
+// 1. Force Node.js to use Google DNS to fix MongoDB connection errors
+import dns from "node:dns";
+dns.setServers(["8.8.8.8", "1.1.1.1"]);
+
 import mongoose from "mongoose";
 
+// 2. Create a function to link our app to the MongoDB database
 const connectDB = async () => {
+  // Try to connect using the secret URL from our .env file
   const conn = await mongoose.connect(process.env.MONGODB_URI);
 
+  // 3. Log a success message so we know it worked!
   console.log(`MongoDB Connected: ${conn.connection.host}`);
 };
 
+// 4. Share this function so we can use it in server.js
 export default connectDB;
